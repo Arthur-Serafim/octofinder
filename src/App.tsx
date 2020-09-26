1ce1abbe6943615a9313584c0e1c46ff73c54436
Arthur-Serafim <dev.seraph.arthur@gmail.com>
2020-09-26 11:29:20 -0300

    Finish router mockup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+      <span>I'm home</span>
+    </div>
+  )
+}
+
+const NotFound = () => {
+  return (
+    <div>
+      <span>I'm not found</span>
+    </div>
+  )
+}
+
+const Profile = () => {
+  return (
+    <div>
+      <span>I'm profile</span>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/404" component={NotFound} />
+        <Route exact path="/perfil/:id" component={Profile} />
+      </Router>
     </div>
   );
 }
